5bc8abb8be9b8558e182d183df0a12c81c98e1f6
knika-starkey <knika787@gmail.com>
2023-02-19 13:49:41 +0200

    first commit

---
+++ ball-class.js
@@ -0,0 +1,94 @@
+const canvas = document.getElementById("canvas");
+const n = 10;
+
+class Ball {
+  constructor(canvas, x = 100, y = 100) {
+    this.ctx = canvas.getContext("2d");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.x = x;
+    this.y = x;
+    this.xSpeed = Math.floor(Math.random() * 10);
+    this.ySpeed = Math.floor(Math.random() * 10);
+    this.colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
+    this.color = this.randColor();
+    //ctx.fillStyle = ctx.clearRect(0, 0, width, height);
+  }
+  randColor() {
+    return this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+  circle(x, y, radius, fillCircle) {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    if (fillCircle) {
+      this.ctx.fill();
+    } else {
+      this.ctx.stroke();
+    }
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.ctx.strokeStyle = this.color;
+    this.circle(this.x, this.y, 5, true);
+  }
+
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
+
+  checkCollision() {
+    if (this.x < 0 || this.x > this.width) {
+      this.xSpeed = -this.xSpeed;
+    }
+    if (this.y < 0 || this.y > this.height) {
+      this.ySpeed = -this.ySpeed;
+    }
+  }
+}
+// let ball = new Ball(canvas);
+// console.log(ball.x, ball.y, ball.xSpeed, ball.ySpeed);
+// ball.draw();
+
+class BallsGame {
+  constructor(balls, canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.balls = balls;
+  }
+
+  clear() {
+    this.ctx.fillStyle = this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  drawBorder() {
+    this.ctx.strokeStyle = "grey";
+    this.ctx.lineWidth = 3;
+    this.ctx.strokeRect(0, 0, this.width, this.height);
+  }
+
+  go() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    console.log(this.balls.length);
+    for (let i = 0; i < this.balls.length; i++) {
+      this.balls[i].draw();
+      this.balls[i].move();
+      this.balls[i].checkCollision();
+    }
+    this.drawBorder();
+  }
+
+  start() {
+    setInterval(this.go.bind(this), 30);
+  }
+}
+
+const balls = [];
+for (let i = 0; i < n; i++) {
+  balls[i] = new Ball(canvas);
+}
+let ballsGame = new BallsGame(balls, canvas);
+ballsGame.drawBorder();
+ballsGame.start();
---
+++ scipt.js
@@ -0,0 +1,69 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var n = 10; // 10 шариков
+
+// конструктор для шариков
+var Ball = function () {
+  this.x = 100;
+  this.y = 100;
+  this.xSpeed = Math.floor(Math.random() * 10);
+  this.ySpeed = Math.floor(Math.random() * 10);
+  ctx.fillStyle = ctx.clearRect(0, 0, width, height);
+};
+// функция рисования шарика
+var circle = function (x, y, radius, fillCircle) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  if (fillCircle) {
+    ctx.fill();
+  } else {
+    ctx.stroke();
+  }
+};
+
+// рисование шарика заданным цветом
+Ball.prototype.draw = function () {
+  ctx.fillStyle = "blue";
+  circle(this.x, this.y, 3, true);
+};
+// let ball = new Ball();
+// console.log(ball.x, ball.y, ball.xSpeed, ball.ySpeed);
+// ball.draw();
+// // движение шарика
+Ball.prototype.move = function () {
+  this.x += this.xSpeed;
+  this.y += this.ySpeed;
+};
+
+// // проверка столкновения шарика и стен - меняем движение на противоположное
+Ball.prototype.checkCollosion = function () {
+  if (this.x < 0 || this.x > width) {
+    this.xSpeed = -this.xSpeed;
+  }
+
+  if (this.y < 0 || this.y > height) {
+    this.ySpeed = -this.ySpeed;
+  }
+};
+
+// рисуем n шариков
+var balls = [];
+for (var i = 0; i < n; i++) {
+  balls[i] = new Ball();
+}
+
+// функция для отрисовки и перемещения шариков
+var go = function () {
+  ctx.clearRect(0, 0, width, height);
+  for (let i = 0; i < 10; i++) {
+    balls[i].draw();
+    balls[i].move();
+    balls[i].checkCollosion();
+  }
+  //ctx.strokeRect(0, 0, width, height);
+};
+
+// Анимируем шарики
+setInterval(go, 30);
